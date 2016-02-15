f9b6d6547984c71ac888c3b22550bcfaf10ff31b
lobsteropteryx <ian.firkin@gmail.com>
2016-02-15 15:55:20 -0500

    Adds generic methods to update keys
    Adds methods to pull root and type level config keys into each service entry in the config, and to add empty arrays for undefined types.

--- ags_publishing_tools/ConfigParser.py
+++ ags_publishing_tools/ConfigParser.py
@@ -4,14 +4,42 @@ import json
 class ConfigParser:
 
     config = None
+    types = ['mapServices', 'gpServices', 'imageServices']
 
     def __init__(self):
         pass
 
     def load_config(self, path_to_config):
         with open(path_to_config) as config_file:
-            self.config = json.load(config_file)
-        return self.config
+            config = json.load(config_file)
+        return self.parse_config(config)
+
+    def parse_config(self, config):
+        parsed = {}
+        for type in self.types:
+            parsed[type] = self.update_keys(config, type)
+        return parsed
+
+    def update_keys(self, config, type):
+        copy = {'services': []}
+        if type in config:
+            extra_keys = self.get_non_type_keys(config, type)
+            copy = config[type].copy()
+            for service in copy['services']:
+                service.update(extra_keys)
+        return copy
+
+    def get_non_type_keys(self, config, type):
+        new_keys = {}
+        for key in config:
+            if key not in self.types:
+                new_keys[key] = config[key]
+            if key == type:
+                for type_key in config[key]:
+                    if type_key not in ['services']:
+                        new_keys[type_key] = config[key][type_key]
+        return new_keys
+
 
     def get_connection_file_path(self, type_key, config_entry):
         connection_key = "connectionFilePath"
--- tests/test_configParser.py
+++ tests/test_configParser.py
@@ -4,7 +4,146 @@ from unittest import TestCase
 from ags_publishing_tools.ConfigParser import ConfigParser
 
 
-class TestSdDraftParser(TestCase):
+class TestConfigParser(TestCase):
+    m = None
+
+    def setUp(self):
+        self.m = ConfigParser()
+
+    def test_empty_config(self):
+        self.assertEqual(self.m.parse_config({}), {
+            'mapServices': {
+                'services': []
+            },
+            'gpServices': {
+                'services': []
+            },
+            'imageServices': {
+                'services': []
+            }
+        })
+
+    def test_no_map_services(self):
+        config = {
+            'gpServices': {
+                'services': [{'input': 'gp'}]
+            },
+            'imageServices': {
+                'services': [{'input': 'image'}]
+            }
+        }
+        self.assertEqual(self.m.parse_config(config), {
+            'mapServices': {
+                'services': []
+            },
+            'gpServices': {
+                'services': [{'input': 'gp'}]
+            },
+            'imageServices': {
+                'services': [{'input': 'image'}]
+            }
+        })
+
+    def test_no_gp_services(self):
+        config = {
+            'mapServices': {
+                'services': [{'input': 'map'}]
+            },
+            'imageServices': {
+                'services': [{'input': 'image'}]
+            }
+        }
+        self.assertEqual(self.m.parse_config(config), {
+            'mapServices': {
+                'services': [{'input': 'map'}]
+            },
+            'gpServices': {
+                'services': []
+            },
+            'imageServices': {
+                'services': [{'input': 'image'}]
+            }
+        })
+
+    def test_no_image_services(self):
+        config = {
+            'gpServices': {
+                'services': [{'input': 'gp'}]
+            },
+            'mapServices': {
+                'services': [{'input': 'map'}]
+            }
+        }
+        self.assertEqual(self.m.parse_config(config), {
+            'mapServices': {
+                'services': [{'input': 'map'}]
+            },
+            'gpServices': {
+                'services': [{'input': 'gp'}]
+            },
+            'imageServices': {
+                'services': []
+            }
+        })
+
+    def test_get_extra_keys(self):
+        config = {
+            'root_level': 'root',
+            'mapServices': {
+                'type_level': 'type',
+                'services': [{
+                    'input': 'map'
+                }]
+            }
+        }
+        self.assertEqual(self.m.get_non_type_keys(config, 'mapServices'), {
+                                                             'root_level': 'root',
+                                                             'type_level': 'type'
+        })
+
+    def test_flattening_keys(self):
+        config = {
+            'root_level': 'root',
+            'mapServices': {
+                'type_level': 'type',
+                'services': [{
+                    'input': 'map'
+                }]
+            }
+        }
+        self.assertEqual(self.m.parse_config(config)['mapServices']['services'][0], {
+                                                             'input': 'map',
+                                                             'root_level': 'root',
+                                                             'type_level': 'type'
+                                                         }
+                         )
+
+class TestSdDraftParser_properties(TestCase):
+    m = None
+
+    def setUp(self):
+        self.m = ConfigParser()
+        self.m.config = {
+            'properties': {
+                'foo': 'bar',
+                'baz': 'quux'
+            },
+            'mapServices': {
+                'services': [
+                    {
+                        'input': '1stInput',
+                    },
+                    {
+                        'input': '2ndInput',
+                        'properties': {
+                            'foo': 'newFoo'
+                        }
+                    }
+                ]
+            }
+        }
+
+class TestSdDraftParser_connection_file_path(TestCase):
     m = None
 
     def setUp(self):
