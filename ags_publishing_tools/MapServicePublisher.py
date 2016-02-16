de3f66edd0bb384a3d533bd94e9f4b8e18a8591e
Ian Firkin <ian.firkin@gisinc.com>
2016-02-16 12:02:43 -0500

    Adds local variable to get connectionFilePath
    getcwd() seems to be changing, trying to set the variable value
    before calling the gp method.

--- ags_publishing_tools/MapServicePublisher.py
+++ ags_publishing_tools/MapServicePublisher.py
@@ -21,12 +21,10 @@ class MapServicePublisher:
     def publish_gp(self, config_entry, filename, sddraft):
 
         if "result" in config_entry:
-            if os.path.isabs(config_entry["result"]):
-                result = config_entry["result"]
-            else:
-                result = self.config_parser.get_full_path(config_entry["result"])
+            result = self.config_parser.get_full_path(config_entry["result"])
         else:
             raise Exception("Result must be included in config for publishing a GP tool")
+        connection_file_path = self.config_parser.get_full_path(config_entry["connectionFilePath"])
 
         self.message("Generating service definition draft for gp tool...")
         arcpy.CreateGPSDDraft(
@@ -34,7 +32,7 @@ class MapServicePublisher:
             out_sddraft=sddraft,
             service_name=config_entry["serviceName"] if "serviceName" in config_entry else filename,
             server_type=config_entry["serverType"] if "serverType" in config_entry else 'ARCGIS_SERVER',
-            connection_file_path=self.config_parser.get_full_path(config_entry["connectionFilePath"]),
+            connection_file_path=connection_file_path,
             copy_data_to_server=config_entry["copyDataToServer"] if "copyDataToServer" in config_entry else False,
             folder_name=config_entry["folderName"] if "folderName" in config_entry else '',
             summary=config_entry["summary"] if "summary" in config_entry else '',
@@ -48,12 +46,11 @@ class MapServicePublisher:
             maxWaitTime=10,
             maxIdleTime=180
         )
-
         return arcpy.mapping.AnalyzeForSD(sddraft)
 
     def publish_mxd(self, config_entry, filename, sddraft):
         mxd = arcpy.mapping.MapDocument(self.config_parser.get_full_path(config_entry["input"]))
-
+        connection_file_path = self.config_parser.get_full_path(config_entry["connectionFilePath"])
         if "workspaces" in config_entry:
             self.set_workspaces(mxd, config_entry["workspaces"])
 
@@ -63,29 +60,28 @@ class MapServicePublisher:
             out_sddraft=sddraft,
             service_name=config_entry["serviceName"] if "serviceName" in config_entry else filename,
             server_type=config_entry["serverType"] if "serverType" in config_entry else 'ARCGIS_SERVER',
-            connection_file_path=self.config_parser.get_full_path(config_entry["connectionFilePath"]),
+            connection_file_path=connection_file_path,
             copy_data_to_server=config_entry["copyDataToServer"] if "copyDataToServer" in config_entry else False,
             folder_name=config_entry["folderName"] if "folderName" in config_entry else None,
             summary=config_entry["summary"] if "summary" in config_entry else None,
             tags=config_entry["tags"] if "tags" in config_entry else None
         )
-
         return arcpy.mapping.AnalyzeForSD(sddraft)
 
     def publish_image_service(self, config_entry, filename, sddraft):
+        connection_file_path = self.config_parser.get_full_path(config_entry["connectionFilePath"])
         self.message("Generating service definition draft for image service...")
         arcpy.CreateImageSDDraft(
             raster_or_mosaic_layer=config_entry["input"],
             out_sddraft=sddraft,
             service_name=config_entry["serviceName"] if "serviceName" in config_entry else filename,
-            connection_file_path=self.config_parser.get_full_path(config_entry["connectionFilePath"]),
+            connection_file_path=connection_file_path,
             server_type=config_entry["serverType"] if "serverType" in config_entry else 'ARCGIS_SERVER',
             copy_data_to_server=config_entry["copyDataToServer"] if "copyDataToServer" in config_entry else False,
             folder_name=config_entry["folderName"] if "folderName" in config_entry else '',
             summary=config_entry["summary"] if "summary" in config_entry else '',
             tags=''
         )
-
         return arcpy.mapping.AnalyzeForSD(sddraft)
 
     def get_output_directory(self, config_entry):
