e6fad94e58d7c86b8610c57cbc6759120065b4c3
Ian Firkin <ian.firkin@gisinc.com>
2016-06-10 17:27:29 -0400

    Fixes git test

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,9 +1,7 @@
+import unittest
 from unittest import TestCase
 from mock import MagicMock, patch
 from slap import git
-
-# mock_subprocess = MagicMock()
-# patch.dict("sys.modules", subprocess=mock_subprocess).start()
 
 
 class TestGitFileManager(TestCase):
@@ -16,10 +14,12 @@ class TestGitFileManager(TestCase):
         git.get_changed_files = MagicMock(return_value=['foo.mxd', 'bar.txt', 'baz.MXD'])
         self.assertEqual(git.build_args(), '-i foo.mxd -i baz.MXD')
 
-    @patch('slap.git.check_output')
-    def test_subprocess_output(self, mock_check_output):
-        expected = ['git',  'diff', '--name-only',  'HEAD', 'HEAD~1']
-        # mock_check_output.return_value = ''
-        # mock_check_output.return_value.returncode = 0
-        git.get_changed_files()
-        mock_check_output.assert_called_once_with(expected)
+    def test_subprocess_output(self, ):
+        mock_check_output = MagicMock()
+        with patch('slap.git.check_output', mock_check_output):
+            expected = ['git',  'diff', '--name-only',  'HEAD', 'HEAD~1']
+            git.get_changed_files()
+            mock_check_output.assert_called_once_with(expected)
+
+if __name__ == "__main__":
+    unittest.main()
