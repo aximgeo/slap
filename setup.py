506b9d13213a53763704ee32ce56603ffdb84a64
Ian Firkin <ian.firkin@gisinc.com>
2016-12-12 00:56:26 -0500

    Adds pypi badge

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.0',
+    version='2.0.0.dev5',
     description='A set of scripts for publishing ESRI services',
     long_description='A set of scripts for publishing ESRI services',
 
