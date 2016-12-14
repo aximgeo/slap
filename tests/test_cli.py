53e0b62dfe926d81668e64c07404178da32593d1
Ian Firkin <ian.firkin@gisinc.com>
2016-12-13 22:41:00 -0500

    Adds tests for missing arguments

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,3 +20,15 @@ class TestCli(TestCase):
     def test_throws_if_no_config(self):
         with self.assertRaises(SystemExit):
             cli.main(['-u', 'user', '-p', 'pass'])
+
+    def test_throws_if_no_username(self):
+        with self.assertRaises(SystemExit):
+            cli.main(['-c', 'config.json', '-p', 'pass'])
+
+    def test_throws_if_no_password(self):
+        with self.assertRaises(SystemExit):
+            cli.main(['-u', 'user', '-c', 'config.json'])
+
+    def test_throws_if_both_git_and_inputs_specified(self):
+        with self.assertRaises(SystemExit):
+            cli.main(['-u', 'user', '-p', 'pass', '-c', 'config.json', '-i', 'some/file', '-g', 'some-hash'])
