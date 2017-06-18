703b65955a51175164c61a5fdbfebba4f0a07788
ifirkin <ian.firkin@homeinsteadinc.com>
2017-06-18 14:05:02 -0700

    Adds coverage module to test dependencies

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['check-manifest'],
-        'test': ['mock', 'coverage', 'pyfakefs', 'pytest'],
+        'test': ['mock', 'coverage', 'pyfakefs', 'pytest', 'pytest-cov'],
     },
 
     # If there are data files included in your packages that need to be
