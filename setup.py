254187a07b834fc240d87d26fe833219742bb4f5
ifirkin <ian.firkin@homeinsteadinc.com>
2017-06-18 17:04:26 -0700

    Adds supported versions for 3.x

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.0.0',
+    version='3.1.0',
     description='A set of scripts for publishing ESRI services',
     long_description='A set of scripts for publishing ESRI services',
 
@@ -51,8 +51,11 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
